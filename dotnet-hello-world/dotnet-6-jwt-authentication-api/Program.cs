f856376174c858464e8c35118f285041a2bbf5ac
sergsoares-personal <sergsoares-personal@sergsoaress-MacBook-Air.local>
2023-09-15 20:38:01 -0300

    dotnet6 with jwt and swagger

---
+++ dotnet-hello-world/dotnet-6-jwt-authentication-api/Program.cs
@@ -0,0 +1,27 @@
+﻿using WebApi.Helpers;
+using WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddCors();
+services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+// Isso aqui configura o JWT (que o mecanismo pra fazer token de autenticacao)
+app.UseMiddleware<JwtMiddleware>();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
+app.Run("http://localhost:4000");
