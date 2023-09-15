64e1e984d2cab12a03aec87b39b2b10d88495d26
sergsoares-personal <sergsoares-personal@sergsoaress-MacBook-Air.local>
2023-09-14 22:09:26 -0300

    add play with json and f#

---
+++ f#-testing-with-dotnet-cli/ConsoleFsharp/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.Text.Json
+
+type Content = {
+  msg: string
+}
+
+type jwtHeader = {
+  typ: string
+  alg: string
+  msg: Content
+}
+
+let msg2 = { msg = "teste"}
+let exHeader = { typ = "JWT"; alg = "HS256"; msg = msg2 }
+let exHeader2 = { typ = "JWT"; alg = "XPTO"; msg = msg2 }
+let exHeader3 = { typ = "JWT"; alg = "New"; msg = msg2 }
+
+let allTypes = ResizeArray<jwtHeader>()
+allTypes.Add exHeader 
+allTypes.Add exHeader2 
+allTypes.Add exHeader3
+
+printfn "%s" (JsonSerializer.Serialize exHeader)
+printfn "%s" (JsonSerializer.Serialize allTypes)
+
---
+++ f#-testing-with-dotnet-cli/draft.md
@@ -0,0 +1,3 @@
+# Create dotnet app with F#
+dotnet new console -lang "F#" -o ConsoleFsharp
+
