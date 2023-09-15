15a38e6cde396095082ea9b115bc9548db7dc0cb
sergsoares-personal <sergsoares-personal@sergsoaress-MacBook-Air.local>
2023-09-14 22:57:00 -0300

    add config for k8s in f#

---
+++ f#-testing-with-dotnet-cli/K8SManager/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.Collections.Generic
+open System
+open k8s
+open k8s.Models
+
+
+let client = Environment.GetEnvironmentVariable("TEST")
+
+printfn "%s" client
+
+// [<EntryPoint>]
+// let main argv =
+//     let client =
+//         Environment.GetEnvironmentVariable("KUBECONFIG")
+//         |> KubernetesClientConfiguration.BuildConfigFromConfigFile
+//         |> fun config -> new Kubernetes(config)
+
+//     let command = (Array.ofList [ "sleep"; "3600" ]) :> IList<string>
+//     let containers =
+//         (Array.ofList
+//             [ V1Container(image = "busybox", command = command, imagePullPolicy = "IfNotPresent", name = "busybox") ])
+//         :> IList<V1Container>
+//     let pod =
+//         V1Pod
+//             (apiVersion = "v1", kind = "Pod", metadata = V1ObjectMeta(Name = "busybox-fscharp"),
+//              spec = V1PodSpec(containers = containers, restartPolicy = "Always"))
+            
+//     client.CreateNamespacedPodWithHttpMessagesAsync(pod, "default").Wait()
+//     printfn "Pod deployed."
+//     0 // return an integer exit code
--- f#-testing-with-dotnet-cli/draft.md
+++ f#-testing-with-dotnet-cli/draft.md
@@ -1,4 +1,6 @@
 # Create dotnet app with F#
 dotnet new console -lang "F#" -o ConsoleFsharp
 dotnet new console -lang "F#" -o EnumTest
+dotnet new console -lang "F#" -o K8SManager
+
 
